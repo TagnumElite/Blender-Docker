489852d7afbab4e4bcc8d7d5f75ed5c4de1c4d8d
Tagan Hoyle <tagnum.elite@gmail.com>
2019-10-12 21:03:10 +0200

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Blender-Docker
  
+Docker images of blender using `ubuntu:latest`
+
+
+## Blender
+
+[![Blender Logo](./assets/blender_logo_socket.svg)](https://blender.org)
+
+### Nightly Builds
+
+Every night a new build is created of the latest successful
+blender 2.80 linux build. This system hasn't been tested
+extensively and may break.
+
+### How to use
+
+You can use this image from either the [Docker Hub](https://hub.docker.com/r/tagnumelite/blender) 
+or [GitHub Packages](https://github.com/TagnumElite/Blender-Docker/packages).
+
+### License
+
+This project is released under the MIT License.
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag tagnumelite/blender:develop tagnumelite/blender:$TRAVIS_TAG
-docker push tagnumelite/blender:$TRAVIS_TAG
+docker tag tagnumelite/blender:develop "tagnumelite/blender:$TRAVIS_TAG"
+docker push "tagnumelite/blender:$TRAVIS_TAG"
 echo "$GITHUB_PASSWORD" | docker login docker.pkg.github.com -u TagnumElite --password-stdin
-docker tag tagnumelite/blender:develop docker.pkg.github.com/tagnumelite/blender-docker/blender:$TRAVIS_TAG
-docker push docker.pkg.github.com/tagnumelite/blender-docker/blender:$TRAVIS_TAG
+docker tag tagnumelite/blender:develop "docker.pkg.github.com/tagnumelite/blender-docker/blender:$TRAVIS_TAG"
+docker push "docker.pkg.github.com/tagnumelite/blender-docker/blender:$TRAVIS_TAG"
